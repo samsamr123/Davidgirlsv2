3d04249cc33ef1925961fc21a53dc4fe9594e0a5
Roisin Wherry <roisinwherry@Roisins-MacBook-Air.local>
2018-11-26 19:53:24 +0000

    update mailgun credentials

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ Pony.options = {
     port: '587',
     enable_starttls_auto: true,
     authentication: 'plain',
-    user_name: 'postmaster@sandboxc5dd2f3c6d4b44e5a5326c1f9326fc65.mailgun.org',
-    password: '944964e9402addd80331083cec29622f-1053eade-57373eed'
+    user_name: 'postmaster@sandboxeaaeb51af3a7415987e6d2c9460029ad.mailgun.org',
+    password: 'bedb200c7b9b7689dfba848b617e9520-1053eade-038dccba'
   }
 }
 
